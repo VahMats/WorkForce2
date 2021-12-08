2fbfee577bee270c811706ef77ad26acfcee2de8
VahMats <vah.mats01@gmail.com>
2021-12-08 17:47:44 +0400

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import RegPage from "./components/RegPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Switch>
+            <Route path="/auth">
+              <LoginPage  />
+            </Route>
+            <Route path="/reg">
+              <RegPage />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default ()=>{
+    return(
+        <div>
+            <input type="text" />
+            <input type="password" />
+        </div>
+    )
+}
