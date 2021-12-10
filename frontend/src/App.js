c5dbf9c40d7ffe1f71790b4e60ff20fbba67c46b
lilith1995 <lilith.mnatsakanyan@gmail.com>
2021-12-10 16:02:01 +0400

    Created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar.js/Navbar';
+import { createContext, useState, useEffect } from 'react';
 import './App.css';
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import LoginPage from "./components/LoginPage";
@@ -6,18 +7,7 @@ import RegPage from "./components/RegPage";
 
 function App() {
   return (
-    <div className="App">
-        <Router>
-          <Switch>
-            <Route path="/auth">
-              <LoginPage  />
-            </Route>
-            <Route path="/reg">
-              <RegPage />
-            </Route>
-          </Switch>
-        </Router>
-    </div>
+    <Navbar />
   );
 }
 
