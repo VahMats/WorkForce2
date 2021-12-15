b3650436bb9a4e8158858e6854648ffdda91c36a
lilith1995 <lilith.mnatsakanyan@gmail.com>
2021-12-15 15:15:17 +0400

    Fixed bugs

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,9 +1,9 @@
 import React, { useContext, useState } from "react";
-import "./UserProfile.css";
 import { Link, NavLink } from "react-router-dom";
 import ManIcon from "../../images/manicon.png";
 import WomanIcon from "../../images/womanicon.png";
 
+import "./UserProfile.css";
 
 
 function UserProfile() {
@@ -25,16 +25,14 @@ function UserProfile() {
                             <li>Lilith@mamble.co</li>
                             <li>  21.12.1995 </li>
                         </ul>
-                        <ul>
+                            </div>
                             <NavLink activeClassName="active-link" to="/userslist">
                                 <li>User List</li>
                             </NavLink>
                             <NavLink activeClassName="active-link" to="/teamlist">
                                 <li>Team List</li>
                             </NavLink>
-                        </ul>
                     </div>
-                </div>
             </nav>
         </>
     );
